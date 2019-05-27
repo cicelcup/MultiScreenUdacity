ad6b0439bb113680ae3abdcfc1ec69ee08774b24
JAPM <cicelcup@gmail.com>
2019-05-27 16:34:38 -0600

    Add list of numbers to the NumbersActivity

--- app/src/main/java/com/example/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/miwok/NumbersActivity.java
@@ -2,6 +2,10 @@ package com.example.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +13,28 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        ArrayList <String> numbers = new ArrayList<String>();
+        numbers.add(getString(R.string.one));
+        numbers.add(getString(R.string.two));
+        numbers.add(getString(R.string.three));
+        numbers.add(getString(R.string.four));
+        numbers.add(getString(R.string.five));
+        numbers.add(getString(R.string.six));
+        numbers.add(getString(R.string.seven));
+        numbers.add(getString(R.string.eight));
+        numbers.add(getString(R.string.nine));
+        numbers.add(getString(R.string.ten));
+
+        LinearLayout listOfNumbers = findViewById(R.id.layout_id);
+
+        int index=0;
+        while (index < numbers.size()){
+            TextView numberElement = new TextView(this);
+            numberElement.setText(numbers.get(index));
+            listOfNumbers.addView(numberElement);
+            index = index +1;
+        }
+
     }
 }
