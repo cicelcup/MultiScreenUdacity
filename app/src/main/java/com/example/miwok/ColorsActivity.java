89b3c1c72d1664156ecefea647ad762bb1b74668
JAPM <cicelcup@gmail.com>
2019-05-28 21:56:55 -0600

    changing the layouts to word (for all the activities) and deleting the xml files

--- app/src/main/java/com/example/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/miwok/ColorsActivity.java
@@ -3,11 +3,13 @@ package com.example.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class ColorsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_colors);
+        setContentView(R.layout.words);
     }
 }
