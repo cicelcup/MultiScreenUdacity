7f44d7c722e2882f92a3e091ce1267c5859cf332
JAPM <cicelcup@gmail.com>
2019-06-11 10:29:54 -0600

    Changing the switch words between different functions
    Signed-off-by: JAPM <cicelcup@gmail.com>

--- app/src/main/java/com/example/miwok/WordFragment.java
+++ app/src/main/java/com/example/miwok/WordFragment.java
@@ -79,25 +79,8 @@ public class WordFragment extends Fragment {
 
         switch (option) {
             case WordAdapter.COLORS:
-                /*Array list where is store the words in english, mitowk, with the image to display and
-        the sound of the file*/
 
-                words.add(new Word(getString(R.string.red), "weṭeṭṭi", R.drawable.color_red,
-                        R.raw.color_red));
-                words.add(new Word(getString(R.string.mustard_yellow), "chiwiiṭә",
-                        R.drawable.color_mustard_yellow, R.raw.color_mustard_yellow));
-                words.add(new Word(getString(R.string.dusty_yellow), "ṭopiisә",
-                        R.drawable.color_dusty_yellow, R.raw.color_dusty_yellow));
-                words.add(new Word(getString(R.string.green), "chokokki", R.drawable.color_green,
-                        R.raw.color_green));
-                words.add(new Word(getString(R.string.brown), "ṭakaakki", R.drawable.color_brown,
-                        R.raw.color_brown));
-                words.add(new Word(getString(R.string.gray), "ṭopoppi", R.drawable.color_gray,
-                        R.raw.color_gray));
-                words.add(new Word(getString(R.string.black), "kululli", R.drawable.color_black,
-                        R.raw.color_black));
-                words.add(new Word(getString(R.string.white), "kelelli", R.drawable.color_white,
-                        R.raw.color_white));
+                addingColorWords(words);
 
         /*work adapter to inflate the layout customize
         This allows to recycle the view improving the performance*/
@@ -106,28 +89,8 @@ public class WordFragment extends Fragment {
                 break;
 
             case WordAdapter.FAMILY:
+                addingFamilyWords(words);
 
-                //Array list where are the words in english, mitowk, the image to display and the audio
-                words.add(new Word(getString(R.string.father), "әpә", R.drawable.family_father,
-                        R.raw.family_father));
-                words.add(new Word(getString(R.string.mother), "әṭa", R.drawable.family_mother,
-                        R.raw.family_mother));
-                words.add(new Word(getString(R.string.son), "angsi", R.drawable.family_son,
-                        R.raw.family_son));
-                words.add(new Word(getString(R.string.daughter), "tune", R.drawable.family_daughter,
-                        R.raw.family_daughter));
-                words.add(new Word(getString(R.string.older_brother), "taachi",
-                        R.drawable.family_older_brother, R.raw.family_older_brother));
-                words.add(new Word(getString(R.string.younger_brother), "chalitti",
-                        R.drawable.family_younger_brother, R.raw.family_younger_brother));
-                words.add(new Word(getString(R.string.older_sister), "teṭe", R.drawable.family_older_sister,
-                        R.raw.family_older_sister));
-                words.add(new Word(getString(R.string.younger_sister), "kolliti",
-                        R.drawable.family_younger_sister, R.raw.family_younger_sister));
-                words.add(new Word(getString(R.string.grandmother), "ama", R.drawable.family_grandmother,
-                        R.raw.family_grandmother));
-                words.add(new Word(getString(R.string.grandfather), "paapa",
-                        R.drawable.family_grandfather, R.raw.family_grandfather));
 
         /*work adapter to inflate the layout customize..
         This allows to recycle the view improving the performance*/
@@ -136,27 +99,7 @@ public class WordFragment extends Fragment {
                 break;
 
             case WordAdapter.NUMBERS:
-                //Array list where are the words in english, mitowk, the image to display and the sound
-                words.add(new Word(getString(R.string.one), "lutti", R.drawable.number_one,
-                        R.raw.number_one));
-                words.add(new Word(getString(R.string.two), "otiiko", R.drawable.number_two,
-                        R.raw.number_two));
-                words.add(new Word(getString(R.string.three), "tolookosu", R.drawable.number_three,
-                        R.raw.number_three));
-                words.add(new Word(getString(R.string.four), "oyyisa", R.drawable.number_four,
-                        R.raw.number_four));
-                words.add(new Word(getString(R.string.five), "massokka",
-                        R.drawable.number_five, R.raw.number_five));
-                words.add(new Word(getString(R.string.six), "temmokka", R.drawable.number_six,
-                        R.raw.number_six));
-                words.add(new Word(getString(R.string.seven), "kenekaku", R.drawable.number_seven,
-                        R.raw.number_seven));
-                words.add(new Word(getString(R.string.eight), "kawinta", R.drawable.number_eight,
-                        R.raw.number_eight));
-                words.add(new Word(getString(R.string.nine), "wo’e", R.drawable.number_nine,
-                        R.raw.number_nine));
-                words.add(new Word(getString(R.string.ten), "na’aacha", R.drawable.number_ten,
-                        R.raw.number_ten));
+                addingNumbersWords(words);
 
         /*work adapter to inflate the layout customize.
         This allows to recycle the view improving the performance*/
@@ -165,27 +108,7 @@ public class WordFragment extends Fragment {
                 break;
 
             default:
-                //Array list where is store the words in english, Miwok word and audio
-                words.add(new Word(getString(R.string.where_are_you_going), "minto wuksus",
-                        R.raw.phrase_where_are_you_going));
-                words.add(new Word(getString(R.string.what_is_your_name), "tinnә oyaase'nә",
-                        R.raw.phrase_what_is_your_name));
-                words.add(new Word(getString(R.string.my_name_is), "oyaaset...",
-                        R.raw.phrase_my_name_is));
-                words.add(new Word(getString(R.string.how_are_you_feeling), "michәksәs?",
-                        R.raw.phrase_how_are_you_feeling));
-                words.add(new Word(getString(R.string.i_m_feeling_good), "kuchi achit",
-                        R.raw.phrase_im_feeling_good));
-                words.add(new Word(getString(R.string.are_you_coming), "әәnәs'aa?",
-                        R.raw.phrase_are_you_coming));
-                words.add(new Word(getString(R.string.yes_I_m_coming), "hәә’ әәnәm",
-                        R.raw.phrase_yes_im_coming));
-                words.add(new Word(getString(R.string.i_m_coming), "әәnәm",
-                        R.raw.phrase_im_coming));
-                words.add(new Word(getString(R.string.let_s_go), "yoowutis",
-                        R.raw.phrase_lets_go));
-                words.add(new Word(getString(R.string.come_here), "әnni'nem",
-                        R.raw.phrase_come_here));
+                addingPhrasesWords(words);
 
         /*work adapter to inflate the layout customize.
         This allows to recycle the view improving the performance*/
@@ -231,6 +154,99 @@ public class WordFragment extends Fragment {
 
     }
 
+    private void addingColorWords(ArrayList<Word> words) {
+        /*Array list where is store the words in english, mitowk, with the image to display and
+        the sound of the file*/
+
+        words.add(new Word(getString(R.string.red), "weṭeṭṭi", R.drawable.color_red,
+                R.raw.color_red));
+        words.add(new Word(getString(R.string.mustard_yellow), "chiwiiṭә",
+                R.drawable.color_mustard_yellow, R.raw.color_mustard_yellow));
+        words.add(new Word(getString(R.string.dusty_yellow), "ṭopiisә",
+                R.drawable.color_dusty_yellow, R.raw.color_dusty_yellow));
+        words.add(new Word(getString(R.string.green), "chokokki", R.drawable.color_green,
+                R.raw.color_green));
+        words.add(new Word(getString(R.string.brown), "ṭakaakki", R.drawable.color_brown,
+                R.raw.color_brown));
+        words.add(new Word(getString(R.string.gray), "ṭopoppi", R.drawable.color_gray,
+                R.raw.color_gray));
+        words.add(new Word(getString(R.string.black), "kululli", R.drawable.color_black,
+                R.raw.color_black));
+        words.add(new Word(getString(R.string.white), "kelelli", R.drawable.color_white,
+                R.raw.color_white));
+    }
+
+    private void addingFamilyWords(ArrayList<Word> words) {
+        //Array list where are the words in english, mitowk, the image to display and the audio
+        words.add(new Word(getString(R.string.father), "әpә", R.drawable.family_father,
+                R.raw.family_father));
+        words.add(new Word(getString(R.string.mother), "әṭa", R.drawable.family_mother,
+                R.raw.family_mother));
+        words.add(new Word(getString(R.string.son), "angsi", R.drawable.family_son,
+                R.raw.family_son));
+        words.add(new Word(getString(R.string.daughter), "tune", R.drawable.family_daughter,
+                R.raw.family_daughter));
+        words.add(new Word(getString(R.string.older_brother), "taachi",
+                R.drawable.family_older_brother, R.raw.family_older_brother));
+        words.add(new Word(getString(R.string.younger_brother), "chalitti",
+                R.drawable.family_younger_brother, R.raw.family_younger_brother));
+        words.add(new Word(getString(R.string.older_sister), "teṭe", R.drawable.family_older_sister,
+                R.raw.family_older_sister));
+        words.add(new Word(getString(R.string.younger_sister), "kolliti",
+                R.drawable.family_younger_sister, R.raw.family_younger_sister));
+        words.add(new Word(getString(R.string.grandmother), "ama", R.drawable.family_grandmother,
+                R.raw.family_grandmother));
+        words.add(new Word(getString(R.string.grandfather), "paapa",
+                R.drawable.family_grandfather, R.raw.family_grandfather));
+    }
+
+    private void addingNumbersWords(ArrayList<Word> words) {
+//Array list where are the words in english, mitowk, the image to display and the sound
+        words.add(new Word(getString(R.string.one), "lutti", R.drawable.number_one,
+                R.raw.number_one));
+        words.add(new Word(getString(R.string.two), "otiiko", R.drawable.number_two,
+                R.raw.number_two));
+        words.add(new Word(getString(R.string.three), "tolookosu", R.drawable.number_three,
+                R.raw.number_three));
+        words.add(new Word(getString(R.string.four), "oyyisa", R.drawable.number_four,
+                R.raw.number_four));
+        words.add(new Word(getString(R.string.five), "massokka",
+                R.drawable.number_five, R.raw.number_five));
+        words.add(new Word(getString(R.string.six), "temmokka", R.drawable.number_six,
+                R.raw.number_six));
+        words.add(new Word(getString(R.string.seven), "kenekaku", R.drawable.number_seven,
+                R.raw.number_seven));
+        words.add(new Word(getString(R.string.eight), "kawinta", R.drawable.number_eight,
+                R.raw.number_eight));
+        words.add(new Word(getString(R.string.nine), "wo’e", R.drawable.number_nine,
+                R.raw.number_nine));
+        words.add(new Word(getString(R.string.ten), "na’aacha", R.drawable.number_ten,
+                R.raw.number_ten));
+    }
+
+    private void addingPhrasesWords(ArrayList<Word> words) {
+//Array list where is store the words in english, Miwok word and audio
+        words.add(new Word(getString(R.string.where_are_you_going), "minto wuksus",
+                R.raw.phrase_where_are_you_going));
+        words.add(new Word(getString(R.string.what_is_your_name), "tinnә oyaase'nә",
+                R.raw.phrase_what_is_your_name));
+        words.add(new Word(getString(R.string.my_name_is), "oyaaset...",
+                R.raw.phrase_my_name_is));
+        words.add(new Word(getString(R.string.how_are_you_feeling), "michәksәs?",
+                R.raw.phrase_how_are_you_feeling));
+        words.add(new Word(getString(R.string.i_m_feeling_good), "kuchi achit",
+                R.raw.phrase_im_feeling_good));
+        words.add(new Word(getString(R.string.are_you_coming), "әәnәs'aa?",
+                R.raw.phrase_are_you_coming));
+        words.add(new Word(getString(R.string.yes_I_m_coming), "hәә’ әәnәm",
+                R.raw.phrase_yes_im_coming));
+        words.add(new Word(getString(R.string.i_m_coming), "әәnәm",
+                R.raw.phrase_im_coming));
+        words.add(new Word(getString(R.string.let_s_go), "yoowutis",
+                R.raw.phrase_lets_go));
+        words.add(new Word(getString(R.string.come_here), "әnni'nem",
+                R.raw.phrase_come_here));
+    }
     /**
      * Code Snippet from Udacity
      * Clean up the media player by releasing its resources.
